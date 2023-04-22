69789b8a44af9a66fa13362b4a2db18d79514b9d
kahgeh <kahgeh@hotmail.com>
2023-04-22 20:05:46 +1000

    add code to extension entry point - search for tabs across windows

---
+++ src/background.ts
@@ -0,0 +1,20 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "SEARCH_TABS") {
+    const query = request.query.toLowerCase();
+    chrome.windows.getAll({ populate: true }, (windows) => {
+      const matchingTabs = [];
+
+      for (const window of windows) {
+        for (const tab of window.tabs) {
+          if (tab.url && tab.url.toLowerCase().includes(query)) {
+            matchingTabs.push(tab);
+          }
+        }
+      }
+
+      sendResponse(matchingTabs);
+    });
+
+    return true; // Required for async sendResponse
+  }
+});
