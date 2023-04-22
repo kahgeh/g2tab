875f7161a90ad35c127a9486f8f09a6da7a69406
kahgeh <kahgeh@hotmail.com>
2023-04-22 20:08:28 +1000

    print out the search result in the popup

---
+++ src/popup.ts
@@ -0,0 +1,26 @@
+// src/popup.ts
+
+const searchInput = document.getElementById("search-input") as HTMLInputElement;
+const resultsList = document.getElementById("results-list") as HTMLUListElement;
+
+searchInput.addEventListener("input", (event) => {
+  const query = searchInput.value.trim();
+  if (!query) {
+    resultsList.innerHTML = "";
+    return;
+  }
+
+  chrome.runtime.sendMessage({ type: "SEARCH_TABS", query }, (tabs) => {
+    resultsList.innerHTML = "";
+
+    for (const tab of tabs) {
+      const listItem = document.createElement("li");
+      const link = document.createElement("a");
+      link.href = tab.url;
+      link.textContent = tab.title || tab.url;
+      link.target = "_blank";
+      listItem.appendChild(link);
+      resultsList.appendChild(listItem);
+    }
+  });
+});
